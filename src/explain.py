63a02b76782cd1b9849075a55bc8300272daf742
kirakosyan04 <kirakosyand04@gmail.com>
2026-08-11 16:18:18 +0400

    Refactor SHAP analysis for pipeline

--- src/explain.py
+++ src/explain.py
@@ -1,13 +1,25 @@
 import shap
+import pandas as pd
 
-def explain_xgboost(model, X_test, preprocessor):
+
+def explain_xgboost(pipeline, X):
+    preprocessor = pipeline.named_steps["preprocessor"]
+    model = pipeline.named_steps["model"]
+
+    X_transformed = preprocessor.transform(X)
     feature_names = preprocessor.get_feature_names_out()
 
+    X_transformed = pd.DataFrame(
+        X_transformed,
+        columns=feature_names,
+        index=X.index
+    )
+
     explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(X_test)
+    shap_values = explainer.shap_values(X_transformed)
 
     shap.summary_plot(
         shap_values,
-        X_test,
+        X_transformed,
         feature_names=feature_names
     )
